17cc7f2f7998cbcabcf4535934ccc66dcc2dd2a5
touyou <fujiyou1121@gmail.com>
2020-09-25 13:56:34 +0900

    :+1: add client info and back/next button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+
 const Index = () => {
-  return <></>;
+  const [currentIndex, setIndex] = useState(0);
+  const mediaArray = ["test", "test2", "test3", "test4"];
+
+  const backIndex = () => {
+    if (currentIndex > 0) {
+      setIndex(currentIndex - 1);
+    } else {
+      setIndex(Math.max(mediaArray.length - 1, 0));
+    }
+  };
+
+  const nextIndex = () => {
+    if (currentIndex < mediaArray.length - 1) {
+      setIndex(currentIndex + 1);
+    } else {
+      setIndex(0);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={backIndex}>Back</button>
+      <p>{mediaArray[currentIndex]}</p>
+      <button onClick={nextIndex}>Next</button>
+    </div>
+  );
 };
 
 export default Index;
