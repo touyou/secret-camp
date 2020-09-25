e06d1bd6e596df7888375bf0260a56775e4ec989
touyou <fujiyou1121@gmail.com>
2020-09-25 18:48:37 +0900

    :wrench: fix api?

--- api/[id].ts
+++ api/[id].ts
@@ -1,18 +1,18 @@
 import { getAlbumUrl } from "../utils/google_api";
+import { NowRequest, NowResponse } from "@vercel/node";
 
-module.exports = async (req, res) => {
+export default async function (req: NowRequest, res: NowResponse) {
   const {
     query: { id },
     headers: { origin },
   } = req;
-
   if (origin) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
-  res.header(
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   const results = await getAlbumUrl(id);
   res.json(results);
-};
+}
