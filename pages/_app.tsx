e226ccc724bf2a2e82538b26341822d9e1d16985
touyou <fujiyou1121@gmail.com>
2020-09-25 22:47:23 +0900

    :+1: change titles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>SecretCamp 2020 Story</title>
+        <title>SecretCamp 2020 Stories</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
